f40e2b5978e3ad03c94182aa757649d330fa38c9
Wahid Sadik <wahidsadikjishan@gmail.com>
2023-05-17 16:23:17 +0600

    Update selection_sort.c

--- Data Structure and Lab/selection_sort.c
+++ Data Structure and Lab/selection_sort.c
@@ -19,17 +19,17 @@ int main() {
 }
 
 void selection_sort(int arr[], int n) {
-  int min_index, temp;
+  int min_num_index, temp;
   for (int i = 0; i < n - 1; i++) {
-    int min_index = i;
+    int min_num_index = i;
     for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[min_index]) {
-        min_index = j;
+      if (arr[j] < arr[min_num_index]) {
+        min_num_index = j;
       }
     }
     temp = arr[i];
-    arr[i] = arr[min_index];
-    arr[min_index] = temp;
+    arr[i] = arr[min_num_index];
+    arr[min_num_index] = temp;
   }
   for (int i = 0; i < n; i++) {
     printf("%d ", arr[i]);
