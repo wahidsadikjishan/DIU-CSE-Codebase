39610c8a28498a8a1e457acd3cdda610f816a69d
Wahid Sadik <wahidsadikjishan@gmail.com>
2023-05-17 17:41:22 +0600

    Update insertion_sort.c

---
+++ Data Structure and Lab/insertion_sort.c
@@ -0,0 +1,36 @@
+/*
+Selection sort
+
+Author: Wahid Sadik
+Created: 17-05-2023
+*/
+
+#include <stdio.h>
+
+void insertion_sort(int arr[], int n);
+
+int main() {
+  int arr[5];
+  int n = 5;
+  printf("Enter 5 numbers: ");
+  for (int i = 0; i < 5; i++) scanf("%d", &arr[i]);
+  printf("Sorted: ");
+  insertion_sort(arr, n);
+  return 0;
+}
+
+void insertion_sort(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    int current_index = i;
+    int current_value = arr[i];
+    while (current_index > 0 && arr[current_index - 1] > current_value) {
+      arr[current_index] = arr[current_index - 1];
+      current_index--;
+    }
+    arr[current_index] = current_value;
+  }
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
