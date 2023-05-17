b655fe3043554590b6b161a0645d2bae7d4fccad
Wahid Sadik <wahidsadikjishan@gmail.com>
2023-05-17 16:13:14 +0600

    Create selection_sort.c

---
+++ Data Structure and Lab/selection_sort.c
@@ -0,0 +1,38 @@
+/*
+Selection sort
+
+Author: Wahid Sadik
+Created: 17-05-2023
+*/
+
+#include <stdio.h>
+
+void selection_sort(int arr[], int n);
+
+int main() {
+  int arr[5];
+  int n = 5;
+  printf("Enter 5 numbers: ");
+  for (int i = 0; i < 5; i++) scanf("%d", &arr[i]);
+  selection_sort(arr, n);
+  return 0;
+}
+
+void selection_sort(int arr[], int n) {
+  int min_index, temp;
+  for (int i = 0; i < n - 1; i++) {
+    int min_index = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min_index]) {
+        min_index = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[min_index];
+    arr[min_index] = temp;
+  }
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
